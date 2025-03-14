4f13eab1ed14d1760591e1cb2bd7bf6cfac120bb
jackson0143 <jacksonn0143@gmail.com>
2025-03-14 22:36:41 +1100

    added the email confirmation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,16 @@
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+// const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-const prompt = "Explain how AI works";
+// const prompt = "Explain how AI works";
 
-const result = await model.generateContent(prompt);
+// const result = await model.generateContent(prompt);
 
-console.log(result.response.text());
+// console.log(result.response.text());
 export default async function PrivatePage() {
   const supabase = await createClient()
 
