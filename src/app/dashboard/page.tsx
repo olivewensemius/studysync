47d7072d25ceb18594e1fee156a9e264585d55e7
jackson0143 <jacksonn0143@gmail.com>
2025-03-14 19:58:29 +1100

    Added supabase, setup base files for server side Auth.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+
+console.log(result.response.text());
+export default async function PrivatePage() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+
+  return <p>Hello {data.user.email}</p>
+}
