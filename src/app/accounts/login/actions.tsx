0598b3210c992674d1ffdd5c529f8674d3eabd10
oliviawensemius <s4101562@student.rmit.edu.au>
2025-03-15 17:32:54 +1100

    Attempting analytics and fixing flashcard logic

--- src/app/accounts/login/actions.tsx
+++ src/app/accounts/login/actions.tsx
@@ -9,7 +9,6 @@ export async function login(formData: FormData) {
   const supabase = await createClient()
 
   // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
